82734717e640469ec471c53fc9e1d9362253fa50
Nycolas Nascimento <dev.nycolas@outlook.com>
2023-09-24 09:12:51 -0300

    Arrumando db postgresql no settings.py

--- fusion/settings.py
+++ fusion/settings.py
@@ -76,20 +76,20 @@ WSGI_APPLICATION = "fusion.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "fusion",
-#         "USER": "nycolas",
-#         "PASSWORD": "123456",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config()
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "fusion",
+        "USER": "nycolas",
+        "PASSWORD": "123456",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 
 # Password validation
