76e9d73d7c65fbfc508f78f2e67e91fa7653bcc7
Nycolas Nascimento <dev.nycolas@outlook.com>
2023-09-24 08:35:20 -0300

    Arrumando arquivos staticos v3

--- fusion/settings.py
+++ fusion/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "Cling"
 ]
 
 ROOT_URLCONF = "fusion.urls"
